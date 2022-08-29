470647c0e38c04d247b354baa07cd3330b894ff5
Philip Levy <philipdlevy@gmail.com>
2022-08-29 17:43:57 -0600

    commit tree b125de545c60e3b8105c6eb4a35ba6e1a7f9e2e0

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,16 +1,73 @@
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+
+// function Counter() {
+//   // read from the Redux store
+//   const items = useSelector((state) => state.items);
+
+//   // gives us the dispatch function to send actions to the Redux store
+//   const dispatch = useDispatch();
+
+//   function handleOnClick() {
+//     // dispatching an action on click
+//     dispatch({ type: "count/increment" });
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleOnClick}>Click</button>
+//       <p>{items.length}</p>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+//////////////////////////////////////////////////////////////////////////
+
+// we can make this code better and easier to use. We can make annd export
+// an increment count function and use that
+
+
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+
+// function incrementCount() {
+//   return { type: "count/increment" };
+// }
+
+// function Counter() {
+//   const items = useSelector((state) => state.items);
+//   const dispatch = useDispatch();
+
+//   function handleOnClick() {
+//     dispatch(incrementCount());
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleOnClick}>Click</button>
+//       <p>{items.length}</p>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+//////////////////////////////////////////////////////////////////////////
+
+// we can move and export the function from the slicer
+
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { incrementCount } from "./counterSlice.js";
 
 function Counter() {
-  // read from the Redux store
   const items = useSelector((state) => state.items);
-
-  // gives us the dispatch function to send actions to the Redux store
   const dispatch = useDispatch();
 
   function handleOnClick() {
-    // dispatching an action on click
-    dispatch({ type: "count/increment" });
+    dispatch(incrementCount());
   }
 
   return (
